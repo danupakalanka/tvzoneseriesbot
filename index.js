f8fd93676b92092f609e5080d24d1df681392f4a
Nadith Dhanula <116380737+nadith-git@users.noreply.github.com>
2022-11-20 22:37:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ const connectToWA = () => {
 				//......................................................Commands..............................................................\\
 
 				case 'arrow': {
-					let start1 = 1
+					let start1 = 01
 					let start2 = 10
 
 					if (!isSUB) return
@@ -101,10 +101,10 @@ const connectToWA = () => {
 
 					conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
 
-					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪①' })
+					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪①' }) // Season - 1
 
 					for (let i = 1; i <= 9; i++) {
-						
+
 						const filenum = start1++
 
 						await conn.sendMessage(config.GROUPJID, {
@@ -114,13 +114,35 @@ const connectToWA = () => {
 						})
 					}
 					for (let i = 10; i <= 23; i++) {
-						
+
 						const filenum = start2++
 
 						await conn.sendMessage(config.GROUPJID, {
 							document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E' + filenum + '_2013_@nadithpro.mkv' },
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S01E' + filenum + ' 2013 .mkv'
+						})
+					}
+					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪②' }) // Season - 2
+
+					for (let i = 1; i <= 9; i++) {
+						
+						const filenum = start1++
+
+						await conn.sendMessage(config.GROUPJID, {
+							document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E0' + filenum + '_2013_@nadithpro.mkv' },
+							mimetype: config.MKVTYPE,
+							fileName: '@nadithpro Arrow S01E0' + filenum + ' 2013 .mkv'
+						})
+					}
+					for (let i = 10; i <= 23; i++) {
+						
+						const filenum = start2++
+
+						await conn.sendMessage(config.GROUPJID, {
+							document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E' + filenum + '_2014_@nadithpro.mkv' },
+							mimetype: config.MKVTYPE,
+							fileName: '@nadithpro Arrow S01E' + filenum + ' 2014 .mkv'
 						})
 					}
 
