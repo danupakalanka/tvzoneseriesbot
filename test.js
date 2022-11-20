f1aeb57f9c46eab48c5950781ecf033191249ff4
Nadith Dhanula <116380737+nadith-git@users.noreply.github.com>
2022-11-20 21:33:52 +0530

    Update test.js

---
+++ test.js
@@ -0,0 +1,359 @@
+// Arrow Season - 1
+
+conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪①' })
+
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E01_2012_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E01 2012 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E02_2012_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E02 2012 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E03_2012_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E03 2012 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E04_2012_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E04 2012 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E05_2012_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E05 2012 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E06_2012_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E06 2012 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E07_2012_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E07 2012 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E08_2012_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E08 2012 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E09_2012_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E09 2012 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E10_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E10 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E11_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E11 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E12_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E12 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E13_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E13 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E14_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E14 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E15_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E15 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E16_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E16 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E17_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E17 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E18_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E18 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E19_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E19 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E20_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E20 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E21_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E21 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E22_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E22 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E23_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S01E23 2013 .mkv'
+})
+
+// Arrow Season - 2
+
+conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪②' })
+
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E01_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E01 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E02_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E02 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E03_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E03 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E04_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E04 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E05_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E05 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E06_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E06 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E07_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E07 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E08_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E08 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E09_2013_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E09 2013 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E10_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E10 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E11_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E11 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E12_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E12 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E13_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E13 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E14_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E14 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E15_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E15 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E16_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E16 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E17_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E17 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E18_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E18 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E19_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E19 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E20_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E20 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E21_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E21 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E22_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E22 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S02E23_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S02E23 2014 .mkv'
+})
+
+// Arrow Season - 3
+
+conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪③' })
+
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E01_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E01 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E02_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E02 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E03_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E03 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E04_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E04 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E05_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E05 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E06_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E06 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E07_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E07 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E08_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E08 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E09_2014_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E09 2014 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E10_2015_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E10 2015 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E11_2015_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E11 2015 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E12_2015_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E12 2015 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E13_2015_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E13 2015 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E14_2015_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E14 2015 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E15_2015_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E15 2015 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E16_2015_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E16 2015 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E17_2015_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E17 2015 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E18_2015_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E18 2015 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E19_2015_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E19 2015 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E20_2015_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E20 2015 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E21_2015_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E21 2015 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E22_2015_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E22 2015 .mkv'
+})
+await conn.sendMessage(config.GROUPJID, {
+    document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S03E23_2015_@nadithpro.mkv' },
+    mimetype: config.MKVTYPE,
+    fileName: '@nadithpro Arrow S03E23 2015 .mkv'
+})
