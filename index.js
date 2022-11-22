c898877cfaaaa4a87a52d350b1ec9f43b6282db6
Nadith Dhanula <116380737+nadith-git@users.noreply.github.com>
2022-11-22 23:49:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S01E0' + filenum + ' 2012 .mkv'
 						})
-					}start1 = 1
+					} start1 = 1
 					for (let i = 10; i <= 23; i++) {
 
 						const filenum = start10++
@@ -119,7 +119,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S01E' + filenum + ' 2013 .mkv'
 						})
-					}start10 = 10
+					} start10 = 10
 
 					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪②' }) // Season - 2
 					for (let i = 1; i <= 9; i++) {
@@ -131,7 +131,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S02E0' + filenum + ' 2013 .mkv'
 						})
-					}start1 = 1
+					} start1 = 1
 					for (let i = 10; i <= 23; i++) {
 
 						const filenum = start10++
@@ -141,7 +141,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S02E' + filenum + ' 2014 .mkv'
 						})
-					}start10 = 10
+					} start10 = 10
 
 					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪③' }) // Season - 3
 					for (let i = 1; i <= 9; i++) {
@@ -153,7 +153,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S03E0' + filenum + ' 2014 .mkv'
 						})
-					}start1 = 1
+					} start1 = 1
 					for (let i = 10; i <= 23; i++) {
 
 						const filenum = start10++
@@ -163,7 +163,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S03E' + filenum + ' 2015 .mkv'
 						})
-					}start10 = 10
+					} start10 = 10
 
 					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪④' }) // Season - 4
 					for (let i = 1; i <= 9; i++) {
@@ -175,7 +175,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S04E0' + filenum + ' 2015 .mkv'
 						})
-					}start1 = 1
+					} start1 = 1
 					for (let i = 10; i <= 23; i++) {
 
 						const filenum = start10++
@@ -185,7 +185,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S04E' + filenum + ' 2016 .mkv'
 						})
-					}start10 = 10
+					} start10 = 10
 
 					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪⑤' }) // Season - 5
 					for (let i = 1; i <= 9; i++) {
@@ -197,7 +197,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S05E0' + filenum + ' 2016 .mkv'
 						})
-					}start1 = 1
+					} start1 = 1
 					for (let i = 10; i <= 23; i++) {
 
 						const filenum = start10++
@@ -207,7 +207,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S05E' + filenum + ' 2017 .mkv'
 						})
-					}start10 = 10
+					} start10 = 10
 
 					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪⑥' }) // Season - 6
 					for (let i = 1; i <= 9; i++) {
@@ -219,7 +219,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S06E0' + filenum + ' 2017 .mkv'
 						})
-					}start1 = 1
+					} start1 = 1
 					for (let i = 10; i <= 23; i++) {
 
 						const filenum = start10++
@@ -229,7 +229,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S06E' + filenum + ' 2018 .mkv'
 						})
-					}start10 = 10
+					} start10 = 10
 
 					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪⑦' }) // Season - 7
 					for (let i = 1; i <= 9; i++) {
@@ -241,7 +241,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S07E0' + filenum + ' 2018 .mkv'
 						})
-					}start1 = 1
+					} start1 = 1
 					for (let i = 10; i <= 22; i++) {
 
 						const filenum = start10++
@@ -251,7 +251,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S07E' + filenum + ' 2019 .mkv'
 						})
-					}start10 = 10
+					} start10 = 10
 
 					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪⑧' }) // Season - 8
 					for (let i = 1; i <= 7; i++) {
@@ -263,7 +263,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S08E0' + filenum + ' 2019 .mkv'
 						})
-					}start1 = 1
+					} start1 = 1
 					for (let i = 8; i <= 9; i++) {
 
 						const filenum = start8++
@@ -273,7 +273,7 @@ const connectToWA = () => {
 							mimetype: config.MKVTYPE,
 							fileName: '@nadithpro Arrow S08E' + filenum + ' 2020 .mkv'
 						})
-					}start8 = 8
+					} start8 = 8
 					await conn.sendMessage(config.GROUPJID, {
 						document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S08E10_2020_@nadithpro.mkv' },
 						mimetype: config.MKVTYPE,
@@ -283,44 +283,209 @@ const connectToWA = () => {
 				}
 					break
 
-				case 'flash': {
-					let start1 = 1
-					let start10 = 10
+				case 'deception': {
 
 					if (!isSUB) return
-
 					conn.sendMessage(from, { react: { text: config.RTYPE3, key: mek.key } })
 
 					await conn.sendMessage(config.GROUPJID, {
-						caption: "Arrow | 2012 - 2020 | English | TV Series | Sinhala Subtitles | TV Zone | " + config.FOOTER,
+						caption: "Deception | 2018 | English | TV Series | Sinhala Subtitles | TV Zone | " + config.FOOTER,
 						footer: config.FOOTER,
-						image: { url: "https://telegra.ph/file/b045071036852fadac8a1.jpg" }
+						image: { url: "https://telegra.ph/file/08e7565e65212394ae0b1.jpg" }
 					})
-
 					conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
 
 					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪①' }) // Season - 1
-
 					for (let i = 1; i <= 9; i++) {
-
 						const filenum = start1++
 
 						await conn.sendMessage(config.GROUPJID, {
-							document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E0' + filenum + '_2012_@nadithpro.mkv' },
+							document: { url: 'https://cloud.nadith.pro/en_tv/Deception_S01E0' + filenum + '_2018_@nadithpro.mkv' },
 							mimetype: config.MKVTYPE,
-							fileName: '@nadithpro Arrow S01E0' + filenum + ' 2012 .mkv'
+							fileName: '@nadithpro Deception S01E0' + filenum + ' 2018 .mkv'
 						})
-					}
-					for (let i = 10; i <= 23; i++) {
+					} start1 = 1
+					for (let i = 10; i <= 13; i++) {
 
 						const filenum = start10++
 
 						await conn.sendMessage(config.GROUPJID, {
-							document: { url: 'https://cloud.nadith.pro/en_tv/Arrow_S01E' + filenum + '_2013_@nadithpro.mkv' },
+							document: { url: 'https://cloud.nadith.pro/en_tv/Deception_S01E' + filenum + '_2018_@nadithpro.mkv' },
 							mimetype: config.MKVTYPE,
-							fileName: '@nadithpro Arrow S01E' + filenum + ' 2013 .mkv'
+							fileName: '@nadithpro Deception S01E' + filenum + ' 2018 .mkv'
 						})
-					}
+					} start10 = 10
+
+					conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+				}
+					break
+
+				case 'got': {
+
+					if (!isSUB) return
+					conn.sendMessage(from, { react: { text: config.RTYPE3, key: mek.key } })
+
+					await conn.sendMessage(config.GROUPJID, {
+						caption: "Game Of Thrones | 2011 - 2019 | 18+ | English | TV Series | Sinhala Subtitles | TV Zone | " + config.FOOTER,
+						footer: config.FOOTER,
+						image: { url: "https://telegra.ph/file/5418c0b778599033406e5.jpg" }
+					})
+					conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪①' }) // Season - 1
+					for (let i = 1; i <= 9; i++) {
+						const filenum = start1++
+
+						await conn.sendMessage(config.GROUPJID, {
+							document: { url: 'https://cloud.nadith.pro/en_tv/Game_Of_Thrones_S01E0' + filenum + '_2011_@nadithpro.mkv' },
+							mimetype: config.MKVTYPE,
+							fileName: '@nadithpro Game Of Thrones S01E0' + filenum + ' 2011 18+ .mkv'
+						})
+					} start1 = 1
+					await conn.sendMessage(config.GROUPJID, {
+						document: { url: 'https://cloud.nadith.pro/en_tv/Game_Of_Thrones_S01E10_2011_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Game Of Thrones S01E10 2011 18+ .mkv'
+					})
+
+					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪②' }) // Season - 2
+					for (let i = 1; i <= 9; i++) {
+						const filenum = start1++
+
+						await conn.sendMessage(config.GROUPJID, {
+							document: { url: 'https://cloud.nadith.pro/en_tv/Game_Of_Thrones_S02E0' + filenum + '_2012_@nadithpro.mkv' },
+							mimetype: config.MKVTYPE,
+							fileName: '@nadithpro Game Of Thrones S02E0' + filenum + ' 2012 18+ .mkv'
+						})
+					} start1 = 1
+					await conn.sendMessage(config.GROUPJID, {
+						document: { url: 'https://cloud.nadith.pro/en_tv/Game_Of_Thrones_S02E10_2012_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Game Of Thrones S02E10 2012 18+ .mkv'
+					})
+
+					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪③' }) // Season - 3
+					for (let i = 1; i <= 9; i++) {
+						const filenum = start1++
+
+						await conn.sendMessage(config.GROUPJID, {
+							document: { url: 'https://cloud.nadith.pro/en_tv/Game_Of_Thrones_S03E0' + filenum + '_2013_@nadithpro.mkv' },
+							mimetype: config.MKVTYPE,
+							fileName: '@nadithpro Game Of Thrones S03E0' + filenum + ' 2013 18+ .mkv'
+						})
+					} start1 = 1
+					await conn.sendMessage(config.GROUPJID, {
+						document: { url: 'https://cloud.nadith.pro/en_tv/Game_Of_Thrones_S03E10_2013_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Game Of Thrones S03E10 2013 18+ .mkv'
+					})
+
+					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪④' }) // Season - 4
+					for (let i = 1; i <= 9; i++) {
+						const filenum = start1++
+
+						await conn.sendMessage(config.GROUPJID, {
+							document: { url: 'https://cloud.nadith.pro/en_tv/Game_Of_Thrones_S04E0' + filenum + '_2014_@nadithpro.mkv' },
+							mimetype: config.MKVTYPE,
+							fileName: '@nadithpro Game Of Thrones S04E0' + filenum + ' 2014 18+ .mkv'
+						})
+					} start1 = 1
+					await conn.sendMessage(config.GROUPJID, {
+						document: { url: 'https://cloud.nadith.pro/en_tv/Game_Of_Thrones_S04E10_2014_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Game Of Thrones S04E10 2014 18+ .mkv'
+					})
+
+					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪⑤' }) // Season - 5
+					for (let i = 1; i <= 9; i++) {
+						const filenum = start1++
+
+						await conn.sendMessage(config.GROUPJID, {
+							document: { url: 'https://cloud.nadith.pro/en_tv/Game_Of_Thrones_S05E0' + filenum + '_2015_@nadithpro.mkv' },
+							mimetype: config.MKVTYPE,
+							fileName: '@nadithpro Game Of Thrones S05E0' + filenum + ' 2015 18+ .mkv'
+						})
+					} start1 = 1
+					await conn.sendMessage(config.GROUPJID, {
+						document: { url: 'https://cloud.nadith.pro/en_tv/Game_Of_Thrones_S05E10_2015_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Game Of Thrones S05E10 2015 18+ .mkv'
+					})
+
+					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪⑥' }) // Season - 6
+					for (let i = 1; i <= 9; i++) {
+						const filenum = start1++
+
+						await conn.sendMessage(config.GROUPJID, {
+							document: { url: 'https://cloud.nadith.pro/en_tv/Game_Of_Thrones_S06E0' + filenum + '_2016_@nadithpro.mkv' },
+							mimetype: config.MKVTYPE,
+							fileName: '@nadithpro Game Of Thrones S06E0' + filenum + ' 2016 18+ .mkv'
+						})
+					} start1 = 1
+					await conn.sendMessage(config.GROUPJID, {
+						document: { url: 'https://cloud.nadith.pro/en_tv/Game_Of_Thrones_S06E10_2016_@nadithpro.mkv' },
+						mimetype: config.MKVTYPE,
+						fileName: '@nadithpro Game Of Thrones S06E10 2016 18+ .mkv'
+					})
+
+					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪⑦' }) // Season - 7
+					for (let i = 1; i <= 7; i++) {
+						const filenum = start1++
+
+						await conn.sendMessage(config.GROUPJID, {
+							document: { url: 'https://cloud.nadith.pro/en_tv/Game_Of_Thrones_S07E0' + filenum + '_2017_@nadithpro.mkv' },
+							mimetype: config.MKVTYPE,
+							fileName: '@nadithpro Game Of Thrones S07E0' + filenum + ' 2017 18+ .mkv'
+						})
+					} start1 = 1
+
+					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪⑧' }) // Season - 8
+					for (let i = 1; i <= 6; i++) {
+						const filenum = start1++
+
+						await conn.sendMessage(config.GROUPJID, {
+							document: { url: 'https://cloud.nadith.pro/en_tv/Game_Of_Thrones_S08E0' + filenum + '_2019_@nadithpro.mkv' },
+							mimetype: config.MKVTYPE,
+							fileName: '@nadithpro Game Of Thrones S08E0' + filenum + ' 2019 18+ .mkv'
+						})
+					} start1 = 1
+
+					conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
+				}
+					break
+
+				case 'kungfupandathedragonknight': {
+
+					if (!isSUB) return
+					conn.sendMessage(from, { react: { text: config.RTYPE3, key: mek.key } })
+
+					await conn.sendMessage(config.GROUPJID, {
+						caption: "Kung Fu Panda The Dragon Knight | 2022 | English | TV Series | Sinhala Subtitles | TV Zone | " + config.FOOTER,
+						footer: config.FOOTER,
+						image: { url: "https://telegra.ph/file/59c587b5ada81f6171b7f.jpg" }
+					})
+					conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
+
+					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪①' }) // Season - 1
+					for (let i = 1; i <= 9; i++) {
+						const filenum = start1++
+
+						await conn.sendMessage(config.GROUPJID, {
+							document: { url: 'https://cloud.nadith.pro/en_tv/Kung_Fu_Panda_The_Dragon_Knight_S01E0' + filenum + '_2022_@nadithpro.mkv' },
+							mimetype: config.MKVTYPE,
+							fileName: '@nadithpro Kung Fu Panda The Dragon Knight S01E0' + filenum + ' 2022 .mkv'
+						})
+					} start1 = 1
+					for (let i = 10; i <= 11; i++) {
+
+						const filenum = start10++
+
+						await conn.sendMessage(config.GROUPJID, {
+							document: { url: 'https://cloud.nadith.pro/en_tv/Kung_Fu_Panda_The_Dragon_Knight_S01E' + filenum + '_2022_@nadithpro.mkv' },
+							mimetype: config.MKVTYPE,
+							fileName: '@nadithpro Kung Fu Panda The Dragon Knight S01E' + filenum + ' 2022 .mkv'
+						})
+					} start10 = 10
 
 					conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
 				}
