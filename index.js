9d03e0feaaff3f71d981e0ce745d46886650b29a
Nadith Dhanula <116380737+danupakalanka@users.noreply.github.com>
2022-11-26 17:01:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -491,43 +491,6 @@ const connectToWA = () => {
 				}
 					break
 
-				case 'kungfupandathedragonknight': {
-
-					if (!isSUB) return
-					conn.sendMessage(from, { react: { text: config.RTYPE3, key: mek.key } })
-
-					await conn.sendMessage(config.GROUPJID, {
-						caption: "Kung Fu Panda The Dragon Knight | 2022 | English | TV Series | Sinhala Subtitles | TV Zone | " + config.FOOTER,
-						footer: config.FOOTER,
-						image: { url: "https://telegra.ph/file/59c587b5ada81f6171b7f.jpg" }
-					})
-					conn.sendMessage(from, { react: { text: config.RTYPE1, key: mek.key } })
-
-					conn.sendMessage(config.GROUPJID, { text: '🅂🄴🄰🅂🄾🄽 ⓪①' }) // Season - 1
-					for (let i = 1; i <= 9; i++) {
-						const filenum = start1++
-
-						await conn.sendMessage(config.GROUPJID, {
-							document: { url: 'https://cloud.nadith.pro/en_tv/Kung_Fu_Panda_The_Dragon_Knight_S01E0' + filenum + '_2022_@nadithpro.mkv' },
-							mimetype: config.MKVTYPE,
-							fileName: '@nadithpro Kung Fu Panda The Dragon Knight S01E0' + filenum + ' 2022 .mkv'
-						})
-					} start1 = 1
-					for (let i = 10; i <= 11; i++) {
-
-						const filenum = start10++
-
-						await conn.sendMessage(config.GROUPJID, {
-							document: { url: 'https://cloud.nadith.pro/en_tv/Kung_Fu_Panda_The_Dragon_Knight_S01E' + filenum + '_2022_@nadithpro.mkv' },
-							mimetype: config.MKVTYPE,
-							fileName: '@nadithpro Kung Fu Panda The Dragon Knight S01E' + filenum + ' 2022 .mkv'
-						})
-					} start10 = 10
-
-					conn.sendMessage(from, { react: { text: config.RTYPE2, key: mek.key } })
-				}
-					break
-
 
 				default:
 
